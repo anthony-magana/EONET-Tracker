2d95547114c34ebca6f2f3b5c94b9628892042e4
Anthony Magana <anthony@anthonys-mbp.home>
2021-05-28 20:41:29 -0700

    makes it so that it closes the info box when clicking outside of it

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,13 +1,16 @@
 
-function LocationInfoBox({info}) {
+function LocationInfoBox({info, clicked}) {
+    
     return (
-        <div className="location-info" >
+        <div className= {clicked ? "location-info" : "hide"}>
             <h2>Event Location Info</h2>
             <ul>
                 <li>ID: <strong>{info.id} </strong></li>
                 <li>Title: <strong>{info.title} </strong></li>
+                <li></li>
             </ul>
-        </div>
+            <p>Click outside of info box to close.</p>
+        </div> 
     )
 }
 
--- src/components/Map.js
+++ src/components/Map.js
@@ -6,28 +6,35 @@ import LocationInfoBox from './LocationInfoBox'
 
 function Map({eventData, center, zoom}) {
     const [locationInfo, setLocationInfo] = useState(null)
-
+    const [clicked, setClicked] = useState(true)
     const markers = eventData.map(ev => {
         if(ev.categories[0].id === 8){
             return <LocationMarker 
                 lat={ev.geometries[0].coordinates[1]} 
                 lng={ev.geometries[0].coordinates[0]} 
-                onClick={() => setLocationInfo({id: ev.id, title: ev.title})}
+                onClick={() => {
+                    setLocationInfo({id: ev.id, title: ev.title}) 
+                    setClicked(true) 
+                }}
+                key={ev.id}
             />
         }
+        
         return null
     })
-
+    
     return (
-        <div className="map">
+        <div className="map" >
             <GoogleMapReact
                 bootstrapURLKeys={{ key:'AIzaSyCKzQ-JkRb4XExRUuCq4z2fLGgXvI5su6U' }}
                 defaultCenter={center}
                 defaultZoom={zoom}
+                onClick={() => setClicked(false)}
             >
                 {markers}
             </GoogleMapReact>
-            {locationInfo && <LocationInfoBox info={locationInfo} /> }
+
+            {clicked ? locationInfo && <LocationInfoBox clicked={clicked} info={locationInfo}/> : null}
         </div>
     )
 }
