abcb803ea6213eddd5e19f797cc662647107a898
Anthony Magana <anthonymagana0705@gmail.com>
2021-11-28 16:50:16 -0800

    updates nasas api link to new url

--- src/App.js
+++ src/App.js
@@ -10,16 +10,14 @@ function App() {
 
   useEffect(() => {
     const fetchEvents = async () => {
-      setLoading(true)
-      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v3/events')
-      const { events } = await res.json()
-
-      setEventData(events)
-      setLoading(false)
+      setLoading(true);
+      const res = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events');
+      const { events } = await res.json();
+      setEventData(events);
+      setLoading(false);
     }
 
     fetchEvents()
-    //console.log(eventData)
   }, [])
 
   return (
